072160caeb59312390e352c951d7c61011acfbd0
Manish kumar <manishk77287@gmail.com>
2024-11-28 13:04:13 +0530

    Done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Button = ({ onClear }) => {
   return (
     <>
-      <button onClick={onClear}>Clear All</button>
+      <button onClick={onClear} className="clear">Clear All</button>
     </>
   );
 };
